d3aea03d7f14b695034ade8c1e0c58763a84d1cd
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-09-21 09:55:03 +0300

    fixed error

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
 
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000)
+
 const app = express();
 const dev = app.get('env') !== 'production';
 
@@ -15,7 +18,7 @@ if (!dev) {
   app.use(express.static(path.resolve(__dirname, 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html')))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
