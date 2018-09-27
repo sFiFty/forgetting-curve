758e6e72c23fa6cf9302b74aa9ca68b73bd28e8e
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-09-27 08:29:59 +0300

    started with blueprint

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Navbar, Button, Alignment } from "@blueprintjs/core";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+              <Navbar.Heading>Blueprint</Navbar.Heading>
+              <Navbar.Divider />
+              <Button className="bp3-minimal" icon="home" text="Home" />
+              <Button className="bp3-minimal" icon="document" text="Files" />
+          </Navbar.Group>
+        </Navbar>
+      </div>
+    )
+  }
+}
