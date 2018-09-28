c3070d25e96130780ce8ca159c5156490850a6bf
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-09-28 07:36:04 +0300

    upgraded react app

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import { Navbar, Button, Alignment } from "@blueprintjs/core";
+import * as A from "@blueprintjs/core";
 
 export default class NavBar extends Component {
   render() {
+    console.log(A)
     return (
       <div>
-        <Navbar>
-          <Navbar.Group align={Alignment.LEFT}>
-              <Navbar.Heading>Blueprint</Navbar.Heading>
-              <Navbar.Divider />
-              <Button className="bp3-minimal" icon="home" text="Home" />
-              <Button className="bp3-minimal" icon="document" text="Files" />
-          </Navbar.Group>
-        </Navbar>
+        <A.Navbar>
+          <A.Navbar.Group>
+              <A.Navbar.Heading>Blueprint</A.Navbar.Heading>
+              <A.Navbar.Divider />
+              <A.Button className="bp3-minimal" icon="home" text="Home" />
+              <A.Button className="bp3-minimal" icon="document" text="Files" />
+          </A.Navbar.Group>
+        </A.Navbar>
       </div>
     )
   }
