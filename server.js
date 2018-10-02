9e36ece8b7444c8065308ad680f6a9fb7a58e098
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-10-02 07:52:49 +0300

    configured connection to mysql

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const mysql = require('mysql');
 let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'test',
+  database : 'forgetting-curve'
+});
+
+connection.connect()
+
+connection.query('SELECT * from users', function (err, rows, fields) {
+  console.log(rows)
+})
+
+connection.end()
